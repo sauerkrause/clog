2ac754529772000a30b41d39cdc7ce285a56b518
Robert Krause <robert.allen.krause@gmail.com>
2015-09-08 23:17:33 -0500

    added ability to crawl directory and generate pages

--- blog.lisp
+++ blog.lisp
@@ -1,3 +1,5 @@
+(require 'cl-fad)
+
 (defun header ()
        "<html>
          <title>Blag</title>
@@ -31,5 +33,29 @@
 (defun post (id title content)
   (format nil "~a~%~a~%~a~%~a~%" (header) (content title content) (navigation id) (footer)))
 
-(let ((post (post 1 "Help" "Lorem ipsum blah blah blag")))
-  (format t post))
+(defun get-posts (directory)
+  (fad:list-directory directory))
+
+(defun write-index (directory i)
+  (format t "Writing index.html")
+  (let ((out (open (format nil "~a/index.html" directory) :direction :output :if-exists :supersede)))
+    (when out
+      (format out (post i "foo" "bar"))
+      (close out))))
+
+(defun write-html (directory)
+  (let ((posts (get-posts (format nil "~a/posts" directory)))
+	(i 0))
+    (dolist (post posts)
+      (let ((out (open (format nil "~a/~d.html" directory i) :direction :output :if-exists :supersede)))
+	(progn
+	  (format t "Writing ~d.html~%" i)
+	  (when (= i 0)
+	    (write-index directory i))
+	  (when out
+	    (format out (post i "foo" "bar"))
+	    (close out))
+	  (setf i (+ i 1)))))))
+
+(write-html "/home/krause/public_html/blog/")
+

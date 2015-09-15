8a61376f3337e1b5979234d0df0b8908102f4cd4
Robert Krause <robert.allen.krause@gmail.com>
2015-09-14 19:48:04 -0500

    now takes markdown files as input and spits them out as blog posts

--- blog.lisp
+++ blog.lisp
@@ -1,61 +1,76 @@
 (require 'cl-fad)
+(require 'cl-markdown)
+(require 'cl-ppcre)
 
-(defun header ()
-       "<html>
-         <title>Blag</title>
-  	 <body>")
+(let ((posts nil))
+  (defun header (title)
+    (format nil "<html>
+         <title>~a</title>
+  	 <body>" title))
 
-(defun content (title content)
-       (format nil "<h1>~a</h1><div class=\"content\">~a</div>" title content))
+  (defun content (content)
+    (format nil "<div class=\"content\">~a</div>" content))
 
-(defun nextp (id)
-  nil)
-(defun prevp (id)
-  nil)
+  (defun nextp (id)
+    (<= (+ id 1) (length posts)))
 
-(defun navigation (id)
-  (let* ((next (when (nextp id)
-		(+ id 1)))
-	(prev (when (prevp id)
-		(- id 1)))
-	(nav (format nil "<div class=\"navigation\">~a ~a</div>"
-		     (if prev
-			 (format nil "<div class=\"previous\"><a href=\"~a.html\">Previous</a></div>" prev)
-		       "")
-		     (if next
-			 (format nil "<div class=\"next\"><a href=\"~a.html\">Next</a></div>" next)
-		       ""))))
-    nav))
+  (defun prevp (id)
+    (> (- id 1) 0))
 
-(defun footer ()
-       "</body></html>")
+  (defun navigation (id)
+    (let* ((next (when (nextp id)
+		   (+ id 1)))
+	   (prev (when (prevp id)
+		   (- id 1)))
+	   (nav (format nil "<div class=\"navigation\">~a ~a</div>"
+			(if prev
+			    (format nil "<div class=\"previous\"><a href=\"~a.html\">Previous</a></div>" prev)
+			  "")
+			(if next
+			    (format nil "<div class=\"next\"><a href=\"~a.html\">Next</a></div>" next)
+			  ""))))
+      nav))
 
-(defun post (id title content)
-  (format nil "~a~%~a~%~a~%~a~%" (header) (content title content) (navigation id) (footer)))
+  (defun footer ()
+    "</body></html>")
 
-(defun get-posts (directory)
-  (fad:list-directory directory))
+  (defun post (id title content)
+    (format nil "~a~%~a~%~a~%~a~%" (header title) (content content) (navigation id) (footer)))
 
-(defun write-index (directory i)
-  (format t "Writing index.html")
-  (let ((out (open (format nil "~a/index.html" directory) :direction :output :if-exists :supersede)))
-    (when out
-      (format out (post i "foo" "bar"))
-      (close out))))
+  (defun get-posts (directory)
+    (fad:list-directory directory))
 
-(defun write-html (directory)
-  (let ((posts (get-posts (format nil "~a/posts" directory)))
-	(i 0))
-    (dolist (post posts)
-      (let ((out (open (format nil "~a/~d.html" directory i) :direction :output :if-exists :supersede)))
-	(progn
-	  (format t "Writing ~d.html~%" i)
-	  (when (= i 0)
-	    (write-index directory i))
-	  (when out
-	    (format out (post i "foo" "bar"))
-	    (close out))
-	  (setf i (+ i 1)))))))
+  (defun get-post-title (path)
+    (with-open-file (input path :direction :input)
+		    (ppcre:regex-replace "^\w*\#*\w*" (read-line input nil) "")))
+
+  (defun slurp-stream (stream)
+    (let ((seq (make-string (file-length stream))))
+      (read-sequence seq stream)
+      seq))
+
+  (defun get-post-body (path)
+    (with-open-file (input path :direction :input)
+		    (let ((contents (slurp-stream input)))
+		      (multiple-value-bind (doc str)
+			  (markdown:markdown contents :stream nil :format :html)
+			str))))
+
+  (defun write-index (directory i post)
+    (format t "Writing index.html")
+    (with-open-file (out (format nil "~a/index.html" directory) :direction :output :if-exists :supersede)
+		    (format out (post i (get-post-title post) (get-post-body post)))))
+
+  (defun write-html (directory)
+    (setf posts (get-posts (format nil "~a/posts" directory)))
+    (let ((i (length posts)))
+      (dolist (post posts)
+	(with-open-file (out (format nil "~a/~d.html" directory i) :direction :output :if-exists :supersede)
+			(progn
+			  (format t "Writing ~d.html~%" i)
+			  (when (= i (length posts))
+			    (write-index directory i post))
+			  (format out (post i (get-post-title post) (get-post-body post)))
+			  (setf i (- i 1))))))))
 
 (write-html "/home/krause/public_html/blog/")
-
